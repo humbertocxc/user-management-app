40c5769da672f2daa38094b0599603e2c470b914
humberto <humberto.gnm98@gmail.com>
2025-11-07 22:06:48 -0300

    fix password rules

--- src/lib/validation/loginSchema.ts
+++ src/lib/validation/loginSchema.ts
@@ -2,5 +2,11 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
   email: z.email("Endereço de e-mail inválido"),
-  password: z.string().min(1, "Senha é obrigatória"),
+  password: z
+    .string()
+    .min(10, "A senha deve ter pelo menos 10 caracteres")
+    .regex(
+      /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/,
+      "A senha deve incluir pelo menos uma letra maiúscula, um número e um caractere especial"
+    ),
 });
--- src/lib/validation/signupSchema.ts
+++ src/lib/validation/signupSchema.ts
@@ -4,8 +4,14 @@ export const signUpSchema = z
   .object({
     name: z.string().min(1, "Nome é obrigatório"),
     email: z.email("Endereço de e-mail inválido"),
-    password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
-    confirmPassword: z.string().min(6, "Confirmação de senha é obrigatória"),
+    password: z
+      .string()
+      .min(10, "A senha deve ter pelo menos 10 caracteres")
+      .regex(
+        /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/,
+        "A senha deve incluir pelo menos uma letra maiúscula, um número e um caractere especial"
+      ),
+    confirmPassword: z.string().min(10, "Confirmação de senha é obrigatória"),
     zipCode: z
       .string()
       .transform((val) => val.replace(/\D/g, ""))
